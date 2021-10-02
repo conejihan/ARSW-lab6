6f40f3285d832546befef07178bb63ce360b6141
Juan Alayon <jkam86425@gmail.com>
2021-10-02 11:19:12 -0500

    errores arreglados

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,6 +4,8 @@ var app = ( function(){
     let totalPoints;
 
     var mapBlueprints = function (data){
+        console.log("map");
+        console.log(data);
         mapList = data.map(function (blueprint){
             let name = blueprint.name;
             let points = blueprint.points.length;
@@ -24,25 +26,62 @@ var app = ( function(){
         }
     };
 
+
+
+    var drawTable = function (apiNameAuthor) {
+        let html = "";
+
+        console.log(privateAuthor);
+        mapList.map(function (blueprint) {
+            html += "<tr>";
+            html += "<td>" + blueprint.name +  "</td>";
+            console.log(blueprint.name);
+            html += "<td>" +blueprint.points+ "</td>";
+            html += "<td> <button type='button' class='btn btn-success' onclick='app.drawBlueprintsPoints(\""+blueprint.name+"\",\""+privateAuthor+"\", \""+apiNameAuthor+"\");'>Open</button></td>"
+            html += "</tr>"
+
+        });
+
+        $("author_name_table").html(privateAuthor+"'s blueprints");
+        $("Table_Content").html(html);
+        $("points").html("Total user points: " + totalPoints);
+    }
+
     var getBlueprints = function (author) {
         privateAuthor = author;
+        let apiNameAuthor = apiclient;
         apiclient.getBlueprintsByAuthor(author, function (error, data){
             mapBlueprints(data);
             calculatePoints();
+            drawTable(apiNameAuthor);
 
         })
 
     };
     var getBluePrintsMock = function (author) {
         privateAuthor = author;
+        let apiNameAuthor = apimock;
         apimock.getBlueprintsByAuthor(author, function (error, data){
             mapBlueprints(data);
             calculatePoints();
+            drawTable(apiNameAuthor);
         })
     };
 
 
     return {
-        getBlueprints, getBluePrintsMock
+        getBlueprints, getBluePrintsMock,
+        drawBlueprintsPoints: function (name, author, apiNameAuthor) {
+            apiNameAuthor.getBlueprintsByNameAndAuthor(name, author, function (error, blueprint){
+                let canvas = $("#canvas")[0];
+                let canvas2d = canvas.getContext("2d");
+                for(let i = 1; i < blueprint.points.length; i++){
+                    canvas2d.moveTo(blueprint.points[i-1].x,blueprint.points[i-1].y);
+                    canvas2d.lineTo(blueprint.points[i].x,blueprint.points[i].y);
+                    canvas2d.stroke();
+                }
+                $("#blueprintname").html("Current blueprint: "+name);
+            })
+        }
     }
 })();
